ec115247c1da347a41dee5c650ddde1eca7541f0
kenkenm43 <kenkenm43@gmail.com>
2021-11-18 12:29:38 +0700

    dotnetAPI

---
+++ server/API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/Products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+  
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+           
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
