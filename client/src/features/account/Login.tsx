18eb9dfb92790e7d2515306e036501e759134830
kenkenm43 <kenkenm43@gmail.com>
2021-12-18 22:17:35 +0700

    Adding React hook form

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,6 +1,4 @@
-import * as React from "react";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
@@ -11,23 +9,19 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Paper } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import agent from "../../app/api/agent";
+import { FieldValues, useForm } from "react-hook-form";
+import { LoadingButton } from "@mui/lab";
 
 export default function Login() {
-  const [values, setValues] = useState({
-    username: "",
-    password: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    agent.Account.login(values);
-  };
-
-  function handleInputChange(event: any) {
-    const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
+  async function submitForm(data: FieldValues) {
+    await agent.Account.login(data);
   }
 
   return (
@@ -47,37 +41,39 @@ export default function Login() {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(submitForm)}
+        noValidate
+        sx={{ mt: 1 }}
+      >
         <TextField
           margin="normal"
           fullWidth
           label="Username"
-          name="username"
           autoFocus
-          onChange={handleInputChange}
-          value={values.username}
+          {...register("username")}
         />
         <TextField
           margin="normal"
           fullWidth
-          name="password"
           label="Password"
           type="password"
-          onChange={handleInputChange}
-          value={values.password}
+          {...register("password")}
         />
         <FormControlLabel
           control={<Checkbox value="remember" color="primary" />}
           label="Remember me"
         />
-        <Button
+        <LoadingButton
+          loading={isSubmitting}
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
         >
           Sign In
-        </Button>
+        </LoadingButton>
         <Grid container>
           <Grid item>
             <Link to="/register">{"Don't have an account? Sign Up"}</Link>
