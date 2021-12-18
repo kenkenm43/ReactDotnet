cc90855ae73ac036a3fd32ca17036a0275e60c7b
kenkenm43 <kenkenm43@gmail.com>
2021-12-18 22:10:41 +0700

    Using controlled component

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -11,17 +11,24 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Paper } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent";
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    agent.Account.login(values);
   };
+
+  function handleInputChange(event: any) {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  }
 
   return (
     <Container
@@ -43,23 +50,21 @@ export default function Login() {
       <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
         <TextField
           margin="normal"
-          required
           fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
+          label="Username"
+          name="username"
           autoFocus
+          onChange={handleInputChange}
+          value={values.username}
         />
         <TextField
           margin="normal"
-          required
           fullWidth
           name="password"
           label="Password"
           type="password"
-          id="password"
-          autoComplete="current-password"
+          onChange={handleInputChange}
+          value={values.password}
         />
         <FormControlLabel
           control={<Checkbox value="remember" color="primary" />}
