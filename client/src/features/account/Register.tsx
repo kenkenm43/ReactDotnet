84a21d0e7fd77d7ff24ff095796bb50ac5a8e65f
kenkenm43 <kenkenm43@gmail.com>
2021-12-19 19:18:03 +0700

    Adding the registration validators

--- client/src/features/account/Register.tsx
+++ client/src/features/account/Register.tsx
@@ -5,30 +5,37 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import {
-  Alert,
-  AlertTitle,
-  List,
-  ListItem,
-  ListItemText,
-  Paper,
-} from "@mui/material";
-import { Link } from "react-router-dom";
+import { Paper } from "@mui/material";
+import { Link, useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { LoadingButton } from "@mui/lab";
 import agent from "../../app/api/agent";
-import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Register() {
-  // const history = useHistory();
-  const [validationErrors, setValidationErrors] = useState([]);
+  const history = useHistory();
   const {
     register,
     handleSubmit,
+    setError,
     formState: { isSubmitting, errors, isValid },
   } = useForm({
     mode: "all",
   });
+
+  function handleApiError(errors: any) {
+    if (errors) {
+      errors.forEach((error: string) => {
+        if (error.includes("Password")) {
+          setError("password", { message: error });
+        } else if (error.includes("Email")) {
+          setError("email", { message: error });
+        } else if (error.includes("Username")) {
+          setError("username", { message: error });
+        }
+      });
+    }
+  }
 
   return (
     <Container
@@ -50,9 +57,12 @@ export default function Register() {
       <Box
         component="form"
         onSubmit={handleSubmit((data) =>
-          agent.Account.register(data).catch((error) =>
-            setValidationErrors(error)
-          )
+          agent.Account.register(data)
+            .then(() => {
+              toast.success("Registration successful - you can now login");
+              history.push("/login");
+            })
+            .catch((error) => handleApiError(error))
         )}
         noValidate
         sx={{ mt: 1 }}
@@ -70,7 +80,13 @@ export default function Register() {
           margin="normal"
           fullWidth
           label="Email address"
-          {...register("email", { required: "Email is required" })}
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^\w+[\w-.]*@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/,
+              message: "Not a valid email address",
+            },
+          })}
           error={!!errors.email}
           helperText={errors?.email?.message}
         />
@@ -79,22 +95,18 @@ export default function Register() {
           fullWidth
           label="Password"
           type="password"
-          {...register("password", { required: "Password is required" })}
+          {...register("password", {
+            required: "Password is required",
+            pattern: {
+              value:
+                /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/,
+              message: "password does not meet complexity requirements",
+            },
+          })}
           error={!!errors.password}
           helperText={errors?.password?.message}
         />
-        {validationErrors.length > 0 && (
-          <Alert severity="error">
-            <AlertTitle>Validation Errors</AlertTitle>
-            <List>
-              {validationErrors.map((error) => (
-                <ListItem key={error}>
-                  <ListItemText>{error}</ListItemText>
-                </ListItem>
-              ))}
-            </List>
-          </Alert>
-        )}
+
         <LoadingButton
           loading={isSubmitting}
           disabled={!isValid}
